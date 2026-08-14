245c488ff412f6e9ec8c95b1754058fe57008a01
Hari kruthik SK <157100195+HARIKRUTHIK@users.noreply.github.com>
2026-08-14 23:30:14 +0530

    Split arithmetic units into individual Verilog files

---
+++ 01_arithmetic_units/cla_4bit.v
@@ -0,0 +1,14 @@
+module cla_4bit(input [3:0] a,b, input cin, output [3:0] sum, output cout);
+wire [3:0] p,g;
+wire c1,c2,c3;
+assign p = a ^ b;
+assign g = a & b;
+assign c1 = g[0] | (p[0] & cin);
+assign c2 = g[1] | (p[1] & c1);
+assign c3 = g[2] | (p[2] & c2);
+assign cout = g[3] | (p[3] & c3);
+assign sum[0] = p[0] ^ cin;
+assign sum[1] = p[1] ^ c1;
+assign sum[2] = p[2] ^ c2;
+assign sum[3] = p[3] ^ c3;
+endmodule
